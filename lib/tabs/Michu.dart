e1cb8cea6f22110f6667c596a1cabcddcae2ff0e
abdusemed musema <abdumussema22@gmail.com>
2023-08-07 08:22:57 +0300

    APK

--- lib/tabs/Michu.dart
+++ lib/tabs/Michu.dart
@@ -107,22 +107,14 @@ class _MichuState extends State<Michu> with SingleTickerProviderStateMixin {
                         bottomRight: Radius.circular(30)),
                   ),
                   child: Column(children: [
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.5,
-                      width: MediaQuery.of(context).size.width * 1,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [Colors.redAccent, Colors.red.shade300],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                        ),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 20),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Container(
-                            height: MediaQuery.of(context).size.height * 0.5,
+                            height: MediaQuery.of(context).size.height * 0.3,
                             width: MediaQuery.of(context).size.width * 1,
                             decoration: BoxDecoration(
                               gradient: LinearGradient(
@@ -137,105 +129,143 @@ class _MichuState extends State<Michu> with SingleTickerProviderStateMixin {
                             ),
                             child: Column(
                               children: [
-                                const Align(
-                                  alignment: Alignment.topCenter,
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(top: 60.0),
-                                    child: Stack(
-                                      alignment: Alignment.center,
-                                      children: [
-                                        // Row(
-                                        //   mainAxisAlignment:
-                                        //       MainAxisAlignment.center,
-                                        //   children: [
-                                        //     Container(
-                                        //       width: 150,
-                                        //       height: 60,
-                                        //       decoration: BoxDecoration(
-                                        //         color: Colors_selector
-                                        //             .primaryColor,
-                                        //         borderRadius:
-                                        //             BorderRadius.circular(20),
-                                        //       ),
-                                        //       child: Center(
-                                        //         child: Text(
-                                        //           "$var1 points",
-                                        //           style: GoogleFonts
-                                        //               .playfairDisplay(
-                                        //             fontSize: 16,
-                                        //             decoration:
-                                        //                 TextDecoration.none,
-                                        //             fontWeight: FontWeight.bold,
-                                        //             color: Colors.black,
-                                        //           ),
-                                        //         ),
-                                        //       ),
-                                        //     ),
-                                        //     const SizedBox(width: 50),
-                                        //     Container(
-                                        //       width: 150,
-                                        //       height: 60,
-                                        //       decoration: BoxDecoration(
-                                        //         color: Colors_selector
-                                        //             .primaryColor,
-                                        //         borderRadius:
-                                        //             BorderRadius.circular(20),
-                                        //       ),
-                                        //       child: Center(
-                                        //         child: Text(
-                                        //           "37 ETB",
-                                        //           style: GoogleFonts
-                                        //               .playfairDisplay(
-                                        //             fontSize: 16,
-                                        //             decoration:
-                                        //                 TextDecoration.none,
-                                        //             fontWeight: FontWeight.bold,
-                                        //             color: Colors.black,
-                                        //           ),
-                                        //         ),
-                                        //       ),
-                                        //     ),
-                                        //   ],
-                                        // ),
-                                        // Container(
-                                        //   decoration: const BoxDecoration(
-                                        //     shape: BoxShape.circle,
-                                        //     color: Colors
-                                        //         .white24, // Adjust the circle background color as needed
-                                        //   ),
-                                        //   padding: const EdgeInsets.all(8),
-                                        //   child: const Icon(
-                                        //     Icons.arrow_forward,
-                                        //     size: 40,
-                                        //     color: Colors.black,
-                                        //   ),
-                                        // )
-                                      ],
+                                Stack(
+                                  alignment: Alignment.center,
+                                  children: [
+                                    // Row(
+                                    //   mainAxisAlignment:
+                                    //       MainAxisAlignment.center,
+                                    //   children: [
+                                    //     Container(
+                                    //       width: 150,
+                                    //       height: 60,
+                                    //       decoration: BoxDecoration(
+                                    //         color: Colors_selector
+                                    //             .primaryColor,
+                                    //         borderRadius:
+                                    //             BorderRadius.circular(20),
+                                    //       ),
+                                    //       child: Center(
+                                    //         child: Text(
+                                    //           "$var1 points",
+                                    //           style: GoogleFonts
+                                    //               .playfairDisplay(
+                                    //             fontSize: 16,
+                                    //             decoration:
+                                    //                 TextDecoration.none,
+                                    //             fontWeight: FontWeight.bold,
+                                    //             color: Colors.black,
+                                    //           ),
+                                    //         ),
+                                    //       ),
+                                    //     ),
+                                    //     const SizedBox(width: 50),
+                                    //     Container(
+                                    //       width: 150,
+                                    //       height: 60,
+                                    //       decoration: BoxDecoration(
+                                    //         color: Colors_selector
+                                    //             .primaryColor,
+                                    //         borderRadius:
+                                    //             BorderRadius.circular(20),
+                                    //       ),
+                                    //       child: Center(
+                                    //         child: Text(
+                                    //           "37 ETB",
+                                    //           style: GoogleFonts
+                                    //               .playfairDisplay(
+                                    //             fontSize: 16,
+                                    //             decoration:
+                                    //                 TextDecoration.none,
+                                    //             fontWeight: FontWeight.bold,
+                                    //             color: Colors.black,
+                                    //           ),
+                                    //         ),
+                                    //       ),
+                                    //     ),
+                                    //   ],
+                                    // ),
+                                    // Container(
+                                    //   decoration: const BoxDecoration(
+                                    //     shape: BoxShape.circle,
+                                    //     color: Colors
+                                    //         .white24, // Adjust the circle background color as needed
+                                    //   ),
+                                    //   padding: const EdgeInsets.all(8),
+                                    //   child: const Icon(
+                                    //     Icons.arrow_forward,
+                                    //     size: 40,
+                                    //     color: Colors.black,
+                                    //   ),
+                                    // )
+                                  ],
+                                ),
+
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Image.asset(
+                                      'assets/images/WhiteLogo.png',
+                                      height: 50,
+                                      width: 90,
                                     ),
                                   ),
                                 ),
                                 Expanded(
-                                  child: Align(
-                                    alignment: Alignment.bottomCenter,
-                                    child: Padding(
-                                        padding: const EdgeInsets.all(8),
-                                        child: Row(children: [
-                                          Icon(
-                                            var1 > 500 && var1 < 799
-                                                ? Icons.star_border
-                                                : var1 > 800 && var1 < 2999
-                                                    ? Icons.emoji_events
-                                                    : var1 > 3000 &&
-                                                            var1 < 15999
-                                                        ? Icons.star_half
-                                                        : var1 > 16000 &&
-                                                                var1 < 24999
-                                                            ? Icons.stars
-                                                            : var1 > 25000
-                                                                ? Icons.star
-                                                                : Icons
-                                                                    .abc, // Replace this with your desired icon
-                                            size: 30,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Align(
+                                      alignment: Alignment.bottomLeft,
+                                      child: Row(children: [
+                                        Icon(
+                                          var1 > 500 && var1 < 799
+                                              ? Icons.star_border
+                                              : var1 > 800 && var1 < 2999
+                                                  ? Icons.emoji_events
+                                                  : var1 > 3000 && var1 < 15999
+                                                      ? Icons.star_half
+                                                      : var1 > 16000 &&
+                                                              var1 < 24999
+                                                          ? Icons.stars
+                                                          : var1 > 25000
+                                                              ? Icons.star
+                                                              : Icons
+                                                                  .abc, // Replace this with your desired icon
+                                          size: 30,
+                                          color: var1 > 500 && var1 < 799
+                                              ? Colors.white
+                                              : var1 > 800 && var1 < 2999
+                                                  ? Colors.brown.shade700
+                                                  : var1 > 3000 && var1 < 15999
+                                                      ? Color.fromARGB(
+                                                          255, 233, 202, 198)
+                                                      : var1 > 16000 &&
+                                                              var1 < 24999
+                                                          ? Colors.deepOrange
+                                                              .shade900
+                                                          : var1 > 25000
+                                                              ? Colors.indigo
+                                                              : Colors_selector
+                                                                  .secondaryColor, // Adjust the icon size as needed
+                                        ),
+                                        const SizedBox(width: 10),
+                                        Text(
+                                          var1 > 500 && var1 < 799
+                                              ? "$member5 Member"
+                                              : var1 > 800 && var1 < 2999
+                                                  ? "$member4 Member"
+                                                  : var1 > 3000 && var1 < 15999
+                                                      ? "$member3 Member"
+                                                      : var1 > 16000 &&
+                                                              var1 < 24999
+                                                          ? "$member2 Member"
+                                                          : var1 > 25000
+                                                              ? "$member1 Member"
+                                                              : "",
+                                          style: GoogleFonts.playfairDisplay(
+                                            fontSize: 20,
+                                            decoration: TextDecoration.none,
                                             color: var1 > 500 && var1 < 799
                                                 ? Colors.white
                                                 : var1 > 800 && var1 < 2999
@@ -251,32 +281,15 @@ class _MichuState extends State<Michu> with SingleTickerProviderStateMixin {
                                                             : var1 > 25000
                                                                 ? Colors.indigo
                                                                 : Colors_selector
-                                                                    .secondaryColor, // Adjust the icon size as needed
+                                                                    .primaryColor,
                                           ),
-                                          const SizedBox(width: 10),
-                                          Text(
-                                            var1 > 500 && var1 < 799
-                                                ? "$member5 Member"
-                                                : var1 > 800 && var1 < 2999
-                                                    ? "$member4 Member"
-                                                    : var1 > 3000 &&
-                                                            var1 < 15999
-                                                        ? "$member3 Member"
-                                                        : var1 > 16000 &&
-                                                                var1 < 24999
-                                                            ? "$member2 Member"
-                                                            : var1 > 25000
-                                                                ? "$member1 Member"
-                                                                : "",
-                                            style: GoogleFonts.playfairDisplay(
-                                                fontSize: 30,
-                                                decoration: TextDecoration.none,
-                                                color: Colors_selector
-                                                    .tertiaryColor),
-                                          ),
-                                        ])),
+                                        )
+                                      ]),
+                                    ),
                                   ),
-                                )
+                                ),
+                                //   ),
+                                // )
                               ],
                             ),
                           ),
