4508162f16f08898d0f2fbd48fa83f2d67b9d04d
abdusemed musema <abdumussema22@gmail.com>
2023-09-11 13:00:08 +0300

    APK

--- lib/tabs/MyProfile.dart
+++ lib/tabs/MyProfile.dart
@@ -2,6 +2,7 @@ import 'package:babstrap_settings_screen/babstrap_settings_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:loyalty_app/colors.dart';
+import 'package:loyalty_app/utils/simple_preference.dart';
 
 class MyProfile extends StatefulWidget {
   const MyProfile({super.key});
@@ -11,6 +12,24 @@ class MyProfile extends StatefulWidget {
 }
 
 class _MyProfileState extends State<MyProfile> {
+  List<String> data = [];
+  Color myColor = Colors.grey;
+  @override
+  void initState() {
+    super.initState();
+    fetchColor();
+  }
+
+  Color hexToColor(String hexString) {
+    print(hexString);
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) {
+      buffer.write('ff');
+    }
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,42 +51,38 @@ class _MyProfileState extends State<MyProfile> {
                   bottomLeft: Radius.circular(30),
                   bottomRight: Radius.circular(30)),
             ),
-            child: Column(
+            child: ListView(
               children: [
                 Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text(
-                    "Profile",
-                    style: GoogleFonts.playfairDisplay(
-                      fontSize: 25,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
+                  padding: const EdgeInsets.symmetric(vertical: 0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [Colors_selector.tertiaryColor, myColor],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter)),
+                    child: BigUserCard(
+                      backgroundColor: myColor,
+                      settingColor: Colors.white,
+                      userName: "James Bond",
+                      userProfilePic: const NetworkImage(
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4lihOt46X7hUMohEzLcHRy5kIMz_Y65T9AQ&usqp=CAU",
+                          scale: 0.1),
+                      cardActionWidget: SettingsItem(
+                        icons: Icons.edit,
+                        iconStyle: IconStyle(
+                          withBackground: true,
+                          borderRadius: 50,
+                          backgroundColor: Colors.yellow[600],
+                        ),
+                        title: "Modify",
+                        subtitle: "Tap to change your data",
+                        onTap: () {
+                          print("OK");
+                        },
+                      ),
                     ),
                   ),
-                ),
-                // const Stack(
-                //   alignment: Alignment.bottomRight,
-                //   children: [
-                //     CircleAvatar(
-                //         radius: 50,
-                //         backgroundImage: NetworkImage(
-                //             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK4c_lXzfD_8hMdLR8LSVOanf2bhADHCwV_mG_XvGHYuP_XLas6x_3ku8eiCz5r3PV7Xg&usqp=CAU")),
-                //     CircleAvatar(
-                //       radius: 15, // Adjust the size of the edit icon circle
-                //       backgroundColor: Colors
-                //           .blue, // Background color for the edit icon circle
-                //       child: Icon(
-                //         Icons.edit, // The edit icon
-                //         color: Colors.white,
-                //         size: 15, // Color of the edit icon
-                //       ),
-                //     ),
-                //   ],
-                // )
-                SimpleUserCard(
-                  userName: "Nom de l'utilisateur",
-                  userProfilePic: NetworkImage(
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK4c_lXzfD_8hMdLR8LSVOanf2bhADHCwV_mG_XvGHYuP_XLas6x_3ku8eiCz5r3PV7Xg&usqp=CAU"),
                 ),
               ],
             ),
@@ -75,5 +90,14 @@ class _MyProfileState extends State<MyProfile> {
         ),
       )),
     );
+  }
+
+  void fetchColor() async {
+    var datas = await SimplePreferences().getData();
+    data = [datas?[0], datas?[1], datas?[2], datas?[3]];
+    setState(() {
+      myColor = hexToColor(data[3]);
+    });
+    print(data);
   }
 }
