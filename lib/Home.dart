cc0b5e50e4fb848549b6ba5281facf03872f7d8c
abdusemed musema <abdumussema22@gmail.com>
2023-09-21 09:20:29 +0300

    APK

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,3 @@
-// import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:circle_nav_bar/circle_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
